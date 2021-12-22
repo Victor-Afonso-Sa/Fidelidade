d52c4736f23ea350b0e987ee1573d3869938f939
Júnior de Oliveira <ellismarjr@hotmail.com>
2021-12-22 17:24:57 -0300

    refactor: move global styles and theme provider to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import { ToastContainer } from "react-toastify";
-import { AppRoutes } from "./routes/Routes";
-import "react-toastify/dist/ReactToastify.min.css";
+import { ThemeProvider } from "styled-components";
+
 import Alert from "./components/Alert";
+import { AppRoutes } from "./routes/Routes";
+
+import { theme } from "./styles/theme";
+import "react-toastify/dist/ReactToastify.min.css";
+import { GlobalStyle } from "./styles/global";
+import "./styles/global.css";
+import "animate.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "antd/dist/antd.variable.min.css";
+
 function App() {
   return (
     <>
+      <GlobalStyle />
       <ToastContainer
         position="top-right"
         autoClose={5000}
@@ -15,7 +26,9 @@ function App() {
         draggable
         pauseOnHover
       />
-      <AppRoutes />
+      <ThemeProvider theme={theme}>
+        <AppRoutes />
+      </ThemeProvider>
       <Alert></Alert>
     </>
   );
