206111b1533f10d537b87f1dfd21f449088a4ce6
Júnior de Oliveira <ellismarjr@hotmail.com>
2022-01-05 11:45:20 -0300

    tests: [MessageCard component] should renders correctly

--- src/components/Cards/MenssageCard/index.tsx
+++ src/components/Cards/MenssageCard/index.tsx
@@ -1,37 +1,33 @@
-// @flow
-import * as React from "react";
-import styled from "styled-components";
+import { MdClear } from "react-icons/md";
+
 import { Cards } from "../../../types/global";
 import { CardsContainer } from "../styles";
-import { MdClear } from "react-icons/md";
+import { MenssageCardBody, MenssageCardHeader } from "./styles";
 interface Props extends Cards {
   title: string;
   content: any;
-  className?: string
+  className?: string;
 }
 
-export const MenssageCardHeader = styled.div`
-  display: flex;
-  align-items: center;
-  font-size: 21px;
-  font-weight: bold;
-`;
-export const MenssageCardBody = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 25px 0;
-`;
-export const MenssageCard = ({ background, color, title, content,className }: Props) => {
+export const MenssageCard = ({
+  background,
+  color,
+  title,
+  content,
+  className,
+}: Props) => {
   return (
-    <CardsContainer hoverable background={background} color={color} className={className}>
+    <CardsContainer
+      hoverable
+      background={background}
+      color={color}
+      className={className}
+    >
       <MenssageCardHeader>
         <span>{title}</span>
-        <MdClear className="hoverable-icon ms-auto p-1" size={28}/>
+        <MdClear className="hoverable-icon ms-auto p-1" size={28} />
       </MenssageCardHeader>
-      <MenssageCardBody>
-         {content}
-      </MenssageCardBody>
+      <MenssageCardBody>{content}</MenssageCardBody>
     </CardsContainer>
   );
 };
---
+++ src/components/Cards/MenssageCard/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const MenssageCardHeader = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 21px;
+  font-weight: bold;
+`;
+
+export const MenssageCardBody = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 25px 0;
+`;
