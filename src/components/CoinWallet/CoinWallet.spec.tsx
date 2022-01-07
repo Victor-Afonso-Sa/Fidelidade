694bd993d6c4b0f2e2168d34f1d64149d66e4363
Júnior de Oliveira <ellismarjr@hotmail.com>
2022-01-07 11:25:16 -0300

    tests: [CoinWallet component]

--- src/components/CoinWallet/CoinWallet.spec.tsx
+++ src/components/CoinWallet/CoinWallet.spec.tsx
@@ -1,5 +1,9 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { mocked } from "jest-mock";
+import * as AlertService from "../Alert";
 import { CoinWallet } from ".";
+import { useWallet } from "../../hooks/useWallet";
 
 jest.mock("../../utils/formatCurrencyPtBr", () => {
   return {
@@ -7,8 +11,19 @@ jest.mock("../../utils/formatCurrencyPtBr", () => {
   };
 });
 
+jest.mock("../../hooks/useWallet");
+
 describe("CoinWallet component", () => {
   it("should renders correctly", () => {
+    const useWalletMocked = mocked(useWallet);
+    useWalletMocked.mockReturnValueOnce({
+      coinsWallet: {
+        amount: null,
+      },
+      canProceedConvertCoinsToMoney: true,
+      handleTransferCoinsWalletToMoneyWallet: jest.fn(),
+    } as any);
+
     render(<CoinWallet />);
 
     expect(
@@ -25,6 +40,15 @@ describe("CoinWallet component", () => {
   });
 
   it("should renders with closed modal", () => {
+    const useWalletMocked = mocked(useWallet);
+    useWalletMocked.mockReturnValueOnce({
+      coinsWallet: {
+        amount: null,
+      },
+      canProceedConvertCoinsToMoney: true,
+      handleTransferCoinsWalletToMoneyWallet: jest.fn(),
+    } as any);
+
     render(<CoinWallet />);
 
     expect(screen.queryByText(/finalizar/i)).not.toBeInTheDocument();
@@ -32,6 +56,15 @@ describe("CoinWallet component", () => {
   });
 
   it("should open modal", () => {
+    const useWalletMocked = mocked(useWallet);
+    useWalletMocked.mockReturnValue({
+      coinsWallet: {
+        amount: 10,
+      },
+      canProceedConvertCoinsToMoney: true,
+      handleTransferCoinsWalletToMoneyWallet: jest.fn(),
+    } as any);
+
     render(<CoinWallet />);
 
     expect(screen.queryByText(/finalizar/i)).not.toBeInTheDocument();
@@ -45,5 +78,42 @@ describe("CoinWallet component", () => {
 
     expect(screen.queryByText(/finalizar/i)).toBeInTheDocument();
     expect(screen.queryByText(/fechar/i)).toBeInTheDocument();
+  });
+
+  it("should", async () => {
+    const useWalletMocked = mocked(useWallet);
+    const handleTransferCoinsWalletToMoneyWalletMock = jest.fn();
+    useWalletMocked.mockReturnValue({
+      coinsWallet: {
+        amount: 100000,
+      },
+      canProceedConvertCoinsToMoney: true,
+      handleTransferCoinsWalletToMoneyWallet:
+        handleTransferCoinsWalletToMoneyWalletMock,
+    } as any);
+
+    jest.spyOn(AlertService, "presentAlert").mockImplementation(() => {
+      return {
+        type: "success",
+        message: "Resgate realizado com sucesso!",
+      };
+    });
+
+    render(<CoinWallet />);
+
+    const openModalButton = screen.getByRole("button", {
+      name: /resgatar/i,
+    });
+
+    userEvent.click(openModalButton);
+
+    const finalizarButton = screen.getByTestId("btn-resgatar");
+    expect(finalizarButton).toBeInTheDocument();
+
+    userEvent.click(finalizarButton);
+
+    await waitFor(async () => {
+      expect(handleTransferCoinsWalletToMoneyWalletMock).toHaveBeenCalled();
+    });
   });
 });
