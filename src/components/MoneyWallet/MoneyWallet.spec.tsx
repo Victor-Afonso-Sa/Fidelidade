43ff1a7c3a6b6ad0acea1ad20490a026841196b1
Júnior de Oliveira <ellismarjr@hotmail.com>
2022-01-07 16:54:09 -0300

    tests: [MoneyWallet Component]

--- src/components/MoneyWallet/MoneyWallet.spec.tsx
+++ src/components/MoneyWallet/MoneyWallet.spec.tsx
@@ -1,6 +1,6 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { mocked } from "jest-mock";
-
+import * as AlertService from "../Alert";
 import { MoneyWallet } from ".";
 import { useWallet } from "../../hooks/useWallet";
 
@@ -9,41 +9,42 @@ jest.mock("../../hooks/useWallet");
 describe("MoneyWallet Component", () => {
   it("renders correctly", () => {
     const useWalletMocked = mocked(useWallet);
-    useWalletMocked.mockReturnValueOnce({
+    useWalletMocked.mockReturnValue({
       moneyWallet: {
-        amount: 0,
-        walletTypeId: 1,
-        amountFormatted: "R$ 0,00",
+        amount: null,
       },
+      canProceedConvertMoneyToCoin: true,
+      handleTransferMoneyWalletToCoinsWallet: jest.fn(),
     } as any);
 
     render(<MoneyWallet />);
 
     expect(screen.getByText("Carteira - Saldo")).toBeInTheDocument();
     expect(screen.getByText("Trocar para moedas")).toBeInTheDocument();
+    expect(screen.getByText("R$ 0,00")).toBeInTheDocument();
   });
 
-  it("renders correctly with amount", () => {
+  it("renders correctly with amount is 10", () => {
     const useWalletMocked = mocked(useWallet);
-    useWalletMocked.mockReturnValueOnce({
+    useWalletMocked.mockReturnValue({
       moneyWallet: {
-        amount: 100,
+        amount: 10,
       },
+      canProceedConvertMoneyToCoin: true,
+      handleTransferMoneyWalletToCoinsWallet: jest.fn(),
     } as any);
 
     render(<MoneyWallet />);
 
-    expect(screen.getByText("R$ 100,00")).toBeInTheDocument();
+    expect(screen.getByText("R$ 10,00")).toBeInTheDocument();
   });
 
   it("should open modal", () => {
     const useWalletMocked = mocked(useWallet);
     useWalletMocked.mockReturnValue({
-      coinsWallet: {
-        amount: 100,
+      moneyWallet: {
+        amount: 10,
       },
-      canProceedConvertCoinsToMoney: true,
-      handleTransferCoinsWalletToMoneyWallet: jest.fn(),
     } as any);
 
     render(<MoneyWallet />);
@@ -59,5 +60,38 @@ describe("MoneyWallet Component", () => {
 
     expect(screen.queryByText(/finalizar/i)).toBeInTheDocument();
     expect(screen.queryByText(/fechar/i)).toBeInTheDocument();
+  });
+
+  it("should call function to convert coins to money", async () => {
+    const useWalletMocked = mocked(useWallet);
+    useWalletMocked.mockReturnValue({
+      moneyWallet: {
+        amount: 50,
+      },
+      canProceedConvertMoneyToCoin: true,
+      handleTransferMoneyWalletToCoinsWallet: jest.fn(),
+    } as any);
+
+    const presentAlertSpy = jest.spyOn(AlertService, "presentAlert");
+
+    render(<MoneyWallet />);
+
+    const openModalButton = screen.getByRole("button", {
+      name: /trocar para moedas/i,
+    });
+
+    fireEvent.click(openModalButton);
+
+    const finalizarButton = screen.getByRole("button", {
+      name: /finalizar/i,
+    });
+
+    expect(finalizarButton).toBeInTheDocument();
+
+    fireEvent.click(finalizarButton);
+
+    await waitFor(async () => {
+      expect(presentAlertSpy).toHaveBeenCalled();
+    });
   });
 });
