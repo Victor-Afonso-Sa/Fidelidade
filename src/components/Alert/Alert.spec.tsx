e3cc925bfca4a5cba7f95ba23263be662f9297c0
Guilherme Cacador <gtech.xavier@gmail.com>
2022-01-11 15:09:35 -0300

    Escrever testes para o alert

--- src/components/Alert/Alert.spec.tsx
+++ src/components/Alert/Alert.spec.tsx
@@ -1,15 +1,27 @@
-import { render, screen } from "@testing-library/react";
-import Alert from ".";
+import { render, screen, waitFor } from "@testing-library/react";
+import * as AlertService from "../Alert";
+import Alert from "../Alert";
 
 describe("Alert component", () => {
-  it("renders correctly", () => {
+  it("render correct message", async () => {
+    const message = "My success message";
     render(<Alert />);
+    await waitFor(() => {
+      AlertService.presentAlert({ message, type: "success" });
+      expect(screen.getByText(message)).toBeInTheDocument();
+    });
+  });
+  it("render info message", async () => {
+    const message = "My success message";
+    render(<Alert />);
+    await waitFor(() => {
+      AlertService.presentAlert({ message, type: "info" });
+    });
 
-    expect(screen.getByText(/texto de exemplo aqui!/i)).toBeInTheDocument();
-    expect(
-      screen.getByRole("button", {
-        name: /ok/i,
-      })
-    ).toBeInTheDocument();
+    // Validate message
+    expect(screen.getByText(message)).toBeInTheDocument();
+
+    // Validate type
+    expect(screen.getByTestId("info")).toBeInTheDocument();
   });
 });
